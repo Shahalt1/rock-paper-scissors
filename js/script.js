c9ae6ce90b57cf775b9f68b4475de5a2b3c80d38
Shahal Thayyil <148101954+Shahalt1@users.noreply.github.com>
2024-07-24 23:58:56 +0530

    Update arrow function

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let isAutoPlaying = false;
 let intervalId;
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function () {
+    intervalId = setInterval(()=> {
       playGame(computerMove());
     }, 1000);
     isAutoPlaying = true;
